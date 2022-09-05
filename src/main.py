4ddc8a8ccb0a459e276376b7cfca9a0eec65a3dc
Guoqiang QI <guoqiang.qi1@gmail.com>
2022-09-05 16:48:04 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,25 +2,22 @@
 '''
 reference to https://huangjj27.gitlab.io/posts/fair-lottery/
 '''
+# -*- coding: utf-8 -*-
 import random
-SAMPLE_COUNT = 10
+SAMPLE_COUNT = 50
 
-# 默认使用系统时间作为种子，增加不可预测性
-# 如果想要得到重复结果
-# 可以使用固定的种子以选取固定的伪随机数数列
-random.seed()
-# random.seed(12345)
+# set number of all users as random seed
+random.seed(98)
 
-sample_titles = []
-for index, line in enumerate(open("sample.txt")):
-    # 初始化奖池
+luck_users = []
+for index, line in enumerate(open("./data/all_users.csv",encoding="utf-8")):
     if index < SAMPLE_COUNT:
-        sample_titles.append(line)
+        luck_users.append(line)
     else:
-        # 以递减的概率替换奖池里的元素
-        # 选择从0到index的一个随机数
         r = random.randint(0, index)
         if r < SAMPLE_COUNT:
-            sample_titles[r] = line
+            luck_users[r] = line
 
-print sample_titles     # 打印中奖结果
+with open("./data/luck_users.csv", "w") as f:
+    for user in luck_users:
+        f.write(user)
